e81eb44056f011d5efe0e6a1c954903d7995673c
Alfie1256 <63114953+Alfie1256@users.noreply.github.com>
2021-04-19 07:49:39 +0100

    General updates
    removes creation of "installed" file and fixes path error.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/you
 sudo chmod a+rx /usr/local/bin/youtube-dl
 #get get.bat
 git clone $1
-cp YouTubeDllSrc/get.bat /get.bat
-cp YouTubeDllSrc/get.sh /get.sh
-#make "installed" file
-touch installed
+cp YouTubeDllSrc/get.bat $2/get.bat
+cp YouTubeDllSrc/get.sh $2/get.sh
+#remove git clone
+rm -rf YoutubeDllSrc
